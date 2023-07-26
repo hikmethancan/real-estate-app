aa292542ad338d83f18e49139077c926ccb0ae50
Hikmethan Can <hikmethann@gmail.com>
2023-07-26 16:31:14 +0300

    searchbar

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,37 @@
 import React from "react";
-import './Hero.css';
+import "./Hero.css";
+
 function Hero() {
   return (
     <section className="hero-wrapper">
       <div className="paddings innerWidth flexCenter hero-container ">
-        <div className="hero-left">
-        left section
+        {/** right Side */}
+        <div className="flexColStart hero-left">
+          <div className="hero-title">
+            <div className="orange-circle"/>
+            <h1>
+              Discover
+              <br />
+              Most Suitable
+              <br />
+              Property
+            </h1>
+          </div>
+
+          <div className="flexColStart hero-desctiption">
+            <span>Find a variety of properties that suit you very easilty</span>
+            <span>Forget all difficulties in finding a residence for you</span>
+          </div>
+          <div className="search-bar">
+                <input type="text"/>
+          </div>
         </div>
+
+        {/** right Side */}
         <div className="flextCenter hero-right">
-            <div className="image-container">
-                <img src="./hero-image.png" alt="" />
-            </div>
+          <div className="image-container">
+            <img src="./hero-image.png" alt="" />
+          </div>
         </div>
       </div>
     </section>
