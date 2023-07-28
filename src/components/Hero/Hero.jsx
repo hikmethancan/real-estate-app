f6d1f6b5477ef4ccc4808ef490192c370241a75b
Hikmethan Can <hikmethann@gmail.com>
2023-07-28 12:06:51 +0300

    CompaniesSetup

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Hero.css";
+import { HiLocationMarker } from "react-icons/hi";
+import  CountUp  from "react-countup";
 
 function Hero() {
   return (
@@ -8,7 +10,7 @@ function Hero() {
         {/** right Side */}
         <div className="flexColStart hero-left">
           <div className="hero-title">
-            <div className="orange-circle"/>
+            <div className="orange-circle" />
             <h1>
               Discover
               <br />
@@ -17,16 +19,39 @@ function Hero() {
               Property
             </h1>
           </div>
-
           <div className="flexColStart hero-desctiption">
-            <span>Find a variety of properties that suit you very easilty</span>
-            <span>Forget all difficulties in finding a residence for you</span>
+            <span className="secondaryText">Find a variety of properties that suit you very easilty</span>
+            <span className="secondaryText">Forget all difficulties in finding a residence for you</span>
           </div>
-          <div className="search-bar">
-                <input type="text"/>
+          <div className="flexCenter search-bar">
+            <HiLocationMarker color="var(--blue)" size={25} />
+            <input type="text" size={30} />
+            <button className="button">Search</button>
+          </div>
+          <div className="flexCenter stats">
+            <div className="flexColStart stat">
+              <span>
+                <CountUp start={88000} end={90000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Premium Products</span>
+            </div>
+            <div className="flexColStart stat">
+              <span>
+            <CountUp start={1950} end={2000} duration={4} />
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Happy Customers</span>
+            </div>
+            <div className="flexColStart stat">
+              <span>
+            <CountUp  end={28}/>
+                <span>+</span>
+              </span>
+              <span className="secondaryText">Award Winnings</span>
+            </div>
           </div>
         </div>
-
         {/** right Side */}
         <div className="flextCenter hero-right">
           <div className="image-container">
---
+++ src/components/Residencies/Residencies.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "swiper/css";
+import "./Residencies.css";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import data from "../../Utils/slider.json";
+
+function Residencies() {
+  return (
+    <section className="r-wrapper">
+      <div className="paddings innerWidth r-container">
+        <div className="r-head flexColStart">
+          <span className="orangeText">Best Choices</span>
+          <span className="primaryText">Populer Residencies</span>
+        </div>
+        <Swiper>
+          {data.map((card, i) => (
+            <SwiperSlide key={i}>
+              <div className="flexColStart r-card">
+                <img src={card.image} alt="home" />
+                <span className="secondaryText r-price">
+                  <span style={{ color: "orange" }}>
+                    $<span>{card.price}</span>
+                  </span>
+                </span>
+                <span className="primaryText">{card.name}</span>
+                <span className="secondaryText">{card.detail}</span>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
+}
+
+export default Residencies;
